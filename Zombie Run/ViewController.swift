ee966b8c6ce33603cd0aff90b514e07825c0e5ff
Aleksy Tylkowski <Aleksy@macbook-pro-aleksy.local>
2017-07-31 11:02:27 +0200

    Initial Commit

---
+++ Zombie Run/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zombie Run
+//
+//  Created by Aleksy Tylkowski on 31.07.2017.
+//  Copyright © 2017 AleksyTylkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
